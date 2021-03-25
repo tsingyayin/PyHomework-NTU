4e2d6d16477dd40f41e45427f76f7f9679f1f062
JUNYI HE <77230422+tsingyayin@users.noreply.github.com>
2021-03-25 08:58:53 +0000

    Create TEST2.py

---
+++ MOOC_TEST/TEST2.py
@@ -0,0 +1,9 @@
+#实际上，到目前为止MOOC上的作业都相当的简单————只要你能用一些后面的方法
+#例如：
+
+date=input()
+datelst=date.split("/")
+print("{}-{:0>2}-{:0>2}".format(datelst[2],datelst[0],datelst[1]))
+
+#上面这三行就能够完成任务。
+#第一个测试就不写了，那玩意太基础了。
